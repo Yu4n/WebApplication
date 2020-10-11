dbe1a96a81b34d594309a923c2c630938b7cfa9c
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-10-11 20:13:42 +0800

    Add topics.html

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -7,6 +7,8 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Home page
+    # Home page.
     path('', views.index, name='index'),
+    # Page that allows all topics.
+    path('topics/', views.topics, name='topics')
 ]
